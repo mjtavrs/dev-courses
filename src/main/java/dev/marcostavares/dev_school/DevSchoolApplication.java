338013589d7b73f023cfce0badb010b89c1bd6c4
Marcos Tavares <mjtdes.md@gmail.com>
2025-03-06 20:18:05 -0300

    Initial edit on pom.xml

---
+++ src/main/java/dev/marcostavares/dev_school/DevSchoolApplication.java
@@ -0,0 +1,13 @@
+package dev.marcostavares.dev_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevSchoolApplication.class, args);
+	}
+
+}
